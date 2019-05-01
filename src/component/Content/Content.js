f3903cd1c5c2c997d1df7d567becdef11089bd3e
GQSM <ms314006@hotmail.com>
2019-05-02 07:45:46 +0800

    [ADD] 替 Route 增加 Switch

--- src/component/Content/Content.js
+++ src/component/Content/Content.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import MyTasks from './MyTasks.js';
 import InProgress from './InProgress.js';
 import Completed from './Completed.js';
@@ -7,9 +7,11 @@ import styles from './index.scss';
 
 const Content = () => (
   <div className={styles.content}>
-    <Route exact path="/" component={MyTasks} />
-    <Route path="/inProgress" component={InProgress} />
-    <Route path="/completed" component={Completed} />
+    <Switch>
+      <Route exact path="/" component={MyTasks} />
+      <Route path="/inProgress" component={InProgress} />
+      <Route path="/completed" component={Completed} />
+    </Switch>
   </div>
 );
 
