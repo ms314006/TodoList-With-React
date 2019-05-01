29e53c5436206f9987bbdbbf6e211b7bc46f88f2
GQSM <ms314006@hotmail.com>
2019-05-01 22:23:25 +0800

    [ADD] 增加 Header 中的三個 Tab ，用來切換路由

---
+++ src/component/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Tab from './Tab.js';
+import styles from './index.scss';
+
+const Header = () => (
+  <div className={styles.header}>
+    <Tab to="./" text="My Tasks" />
+    <Tab to="./inProgress" text="In Progress" />
+    <Tab to="./completed" text="Completed" />
+  </div>
+);
+
+export default Header;
---
+++ src/component/Header/Tab.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Link } from 'react-router-dom';
+import styles from './index.scss';
+
+const Tab = (props) => {
+  const { to, text, } = props;
+  return (
+    <Route
+      exact
+      path="/"
+    >
+      {
+        <Link to={to}>
+          <button
+            type="button"
+            className={styles.tab}
+          >
+            {text}
+          </button>
+        </Link>
+      }
+    </Route>
+  );
+};
+
+Tab.propTypes = {
+  to: PropTypes.string,
+  text: PropTypes.string,
+};
+
+Tab.defaultProps = {
+  to: '/',
+  text: '',
+};
+
+export default Tab;
