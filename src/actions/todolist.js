c7c67662c1c6bcd56de7e629c127c0ff2db7a44d
GQSM <ms314006@hotmail.com>
2019-05-02 21:07:33 +0800

    [ADD] add todolist action

--- src/actions/todolist.js
+++ src/actions/todolist.js
@@ -1,8 +1,27 @@
-export const CHANGE_PAGE_TYPE = 'CHANGE_PAGE_TYPE';
+export const ADD_TODOLIST = 'ADD_TODOLIST';
 
-export const changePageType = type => ({
-  type: CHANGE_PAGE_TYPE,
+export const addTodolist = todolist => ({
+  type: ADD_TODOLIST,
   payload: {
-    pageType: type,
+    ...todolist,
+  },
+});
+
+export const MODIFY_TODOLIST = 'MODIFY_TODOLIST';
+
+export const modifyTodoList = todolist => ({
+  type: MODIFY_TODOLIST,
+  payload: {
+    ...todolist,
+  },
+});
+
+export const CHANGE_TODOLIST_STATUS = 'CHANGE_TODOLIST_STATUS';
+
+export const changeTodolistStatus = ({ key, status, }) => ({
+  type: CHANGE_TODOLIST_STATUS,
+  payload: {
+    key,
+    status,
   },
 });
