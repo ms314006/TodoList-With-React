b2a69dfd8d9a334d41695d111b802b0cc31ea7b7
GQSM <ms314006@hotmail.com>
2019-05-02 22:30:45 +0800

    [ADD] add Reducer victory changeContentType

---
+++ src/reducer/todolist.js
@@ -0,0 +1,40 @@
+import {
+  CHANGE_CONTENT_TYPE,
+  ADD_TODOLIST,
+  MODIFY_TODOLIST,
+  CHANGE_TODOLIST_STATUS
+} from '../actions/todolist';
+
+const initState = {
+  contentType: 'My Tasks',
+  todolist: [{
+    key: 1,
+    import: false,
+    completed: false,
+    title: '打文章',
+    deadlineDate: '2019-05-03',
+    deadlineTime: '05:30',
+    file: '文章列表.jpg',
+    description: '輕鬆打一波',
+  }],
+};
+
+const todolist = (state = initState, action) => {
+  switch (action.type) {
+    case CHANGE_CONTENT_TYPE:
+      return {
+        ...state,
+        contentType: action.payload.contentType,
+      };
+    case ADD_TODOLIST:
+      return { ...state, };
+    case MODIFY_TODOLIST:
+      return { ...state, };
+    case CHANGE_TODOLIST_STATUS:
+      return { ...state, };
+    default:
+      return state;
+  }
+};
+
+export default todolist;
