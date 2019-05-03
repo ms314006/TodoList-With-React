4bb438ed8c5e1fc14f962542710f109872b89a00
GQSM <ms314006@hotmail.com>
2019-05-03 22:43:49 +0800

    [CHANGE] Move Tab component to new folder

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import Tab from './Tab.js';
+import Tab from './Tab';
 import { changeContentType } from '../../actions/todolist';
 import styles from './index.scss';
 
