39043e2f1f588def79e72005de3bcdd9c4a520fb
GQSM <ms314006@hotmail.com>
2019-05-04 09:33:16 +0800

    [ADD] 增加 Tasks component 的 footer component 裡面有 cancel / save 的按鈕

---
+++ src/component/Task/Footer/Footer.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './index.scss';
+
+const Footer = (props) => {
+  const { switchAddTask, } = props;
+  return (
+    <div className={styles.task_footer}>
+      <button
+        type="button"
+        className={`${styles.cancel_button} ${styles.footer_button}`}
+        onClick={() => { switchAddTask(); }}
+      >
+        Ｘ Cancel
+      </button>
+      <button
+        type="button"
+        className={`${styles.save_button} ${styles.footer_button}`}
+        onClick={() => { switchAddTask(); }}
+      >
+        ＋ Add Tasks
+      </button>
+    </div>
+  );
+};
+
+
+Footer.propTypes = {
+  switchAddTask: PropTypes.func,
+};
+
+Footer.defaultProps = {
+  switchAddTask: () => { console.log('Task'); },
+};
+
+export default Footer;
