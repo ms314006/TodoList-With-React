8845783343fa07a01bcd24bfddbac44b1c02c055
GQSM <ms314006@hotmail.com>
2019-05-04 21:34:01 +0800

    [CHANGE] 增加 header component 的 props ，讓編輯 task 時可以共用他，另外也增加了判斷 import 和 import 時 style 的變化

--- src/component/Task/Header/Header.js
+++ src/component/Task/Header/Header.js
@@ -2,29 +2,86 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './index.scss';
 
-const Header = () => {
+const Header = (props) => {
+  const { type, list, detailDataRow, } = props;
   return (
-    <div className={styles.task_header}>
-      <div className={styles.complete_check_block}>
-        <i className="far fa-square" />
-        {/*<i className={`fas fa-check-square ${styles.check_completed}`} />*/}
+    <div className={list.import ? styles.import_task_header : styles.task_header}>
+      <div className={type === 'display' ? styles.display_main_data : styles.main_data}>
+        <div className={styles.complete_check_block}>
+          {
+            list.completed
+              ? <i className={`fas fa-check-square ${styles.check_completed}`} />
+              : <i className="far fa-square" />
+          }
+        </div>
+        <div className={styles.title_block}>
+          {type === 'display'
+            ? (
+              <span
+                className={`${styles.task_title} ${
+                  list.completed ? styles.completed_task_title : ''
+                } ${
+                  list.import ? styles.import_task_header : ''
+                }`}
+              >
+                {list.title}
+              </span>
+            )
+            : (
+              <input
+                type="text"
+                className={styles.task_title}
+                placeholder="Type Something Here…"
+              />
+            )
+          }
+        </div>
+        <div>
+          {
+            list.import
+              ? <i className={`fas fa-star ${list.import ? styles.check_import : ''}`} />
+              : <i className="far fa-star" />
+          }
+        </div>
+        <div>
+          <i className={`fas fa-pen ${type === 'display' ? '' : styles.edit_tasks_icon}`} />
+        </div>
       </div>
-      <div>
-        <input
-          type="text"
-          className={styles.input_task_name}
-          placeholder="Type Something Here…"
-        />
-      </div>
-      <div>
-        <i className="far fa-star" />
-        {/*<i className={`fas fa-star ${styles.check_import}`} />*/}
-      </div>
-      <div>
-        <i className={`fas fa-pen ${styles.edit_tasks_icon}`} />
-      </div>
+      {detailDataRow || null}
     </div>
   );
+};
+
+Header.propTypes = {
+  type: PropTypes.string,
+  list: PropTypes.shape({
+    id: PropTypes.number,
+    onEdit: PropTypes.bool,
+    import: PropTypes.bool,
+    completed: PropTypes.bool,
+    title: PropTypes.string,
+    deadlineDate: PropTypes.string,
+    deadlineTime: PropTypes.string,
+    file: PropTypes.string,
+    description: PropTypes.string,
+  }),
+  detailDataRow: PropTypes.shape(),
+};
+
+Header.defaultProps = {
+  type: '',
+  list: {
+    id: 0,
+    onEdit: false,
+    import: false,
+    completed: false,
+    title: '',
+    deadlineDate: '',
+    deadlineTime: '',
+    file: '',
+    description: '',
+  },
+  detailDataRow: <></>,
 };
 
 export default Header;
