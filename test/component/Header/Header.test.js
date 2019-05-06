a934dc75a6c0f09a5807de7a2e86dd4e7c1d69eb
GQSM <ms314006@hotmail.com>
2019-05-06 21:42:03 +0800

    [ADD] 完成對 Header 的 test case

---
+++ test/component/Header/Header.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+import { toBeInTheDocument } from 'jest-dom';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import { PureHeader as Header } from '../../../src/component/Header';
+
+expect.extend({ toBeInTheDocument, });
+
+const mockStore = configureMockStore();
+const store = mockStore({});
+
+const props = {
+  changeContentType: jest.fn(),
+  contentType: 'My Tasks',
+};
+
+describe('test <Header />', () => {
+  afterEach(cleanup);
+
+  test('確認是否正常 render ', () => {
+    const { container, } = render(
+      <Provider store={store}>
+        <Header {...props} />
+      </Provider>
+    );
+    expect(container.querySelector('[class="header"]')).toBeInTheDocument();
+
+    // 有三個 tab
+    expect(container.querySelectorAll('button').length).toBe(3);
+  });
+});
